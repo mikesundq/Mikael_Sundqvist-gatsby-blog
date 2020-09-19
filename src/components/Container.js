a033c1671bc44164e8b1250aa49a4f3c704c3e72
mikesundq <55479605+mikesundq@users.noreply.github.com>
2020-09-19 09:31:02 +0200

    added nav

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,9 +1,11 @@
 import {ContainerWrapper} from "../elements"
 import React from "react"
+import { Nav } from "../components"
 
 export const Container = ({children}) => {
     return (
         <ContainerWrapper>
+            <Nav/>
             {children}
         </ContainerWrapper>
     )

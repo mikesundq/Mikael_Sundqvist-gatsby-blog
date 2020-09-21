030371420e7966bd8a7c27277cb4a6e1d67e342d
mikesundq <55479605+mikesundq@users.noreply.github.com>
2020-09-21 20:51:13 +0200

    added allPosts

--- src/templates/allPosts.js
+++ src/templates/allPosts.js
@@ -7,14 +7,64 @@ import {
     FeatureImage,
     Pagination
 } from "../components"
-import { H1, P } from "../elements"
+import { H1 } from "../elements"
 
 const allPosts = ({pageContext, data}) => {
     const {currentPage, numPages} = pageContext
     const isFirst = currentPage === 1
-    const isLast =  currenPage === numPages
-    const prevPage = currentPage - 1 === 1 ? "/" : `/${currenPage - 1}`
-    const nextPage = `/${currenPage + 1}`
+    const isLast =  currentPage === numPages
+    const prevPage = currentPage - 1 === 1 ? "/" : `/${currentPage - 1}`
+    const nextPage = `/${currentPage + 1}`
 
     const posts = data.allMdx.edges
+
+    return(
+        <Container>
+            <FeatureImage/>
+            <Content>
+                <H1 margin="0 0 1rem 0">
+                My Glorious Blog
+                </H1>
+                {posts.map(post => (
+                    <ContentCard
+                        key={post.node.frontmatter.slug}
+                        date={post.node.frontmatter.date}
+                        title={post.node.frontmatter.title}
+                        excerpt={post.node.frontmatter.excerpt}
+                        slug={post.node.frontmatter.slug}
+                        />
+                ))}
+            </Content>
+            <Pagination
+                isFirst={isFirst}
+                isLast={isLast}
+                prevPage={prevPage}
+                nextPage={nextPage}
+            />
+    </Container>
+    )
+
 }
+
+export const pageQuery = graphql`
+
+    query AllPostsQuery($skip : Int!, $limit: Int!) {
+        allMdx(
+            sort: { fields: frontmatter___date, order: DESC }
+            skip: $skip
+            limit: $limit
+          ) {
+            edges {
+              node {
+                frontmatter {
+                  slug
+                  title
+                  date(formatString: "MMMM DD, YYYY")
+                  excerpt
+                }
+              }
+            }
+          }
+    }
+`
+export default allPosts
